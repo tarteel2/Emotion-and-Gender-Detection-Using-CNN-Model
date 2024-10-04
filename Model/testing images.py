e6ff9e63ccbacf33d426c8805dd1685f4ae0b800
tarteel2 <tuti.alkaraan.22@gmail.com>
2024-10-04 15:40:31 +0100

    Solution

---
+++ Model/testing images.py
@@ -0,0 +1,69 @@
+#Import Dependencies
+import cv2
+import numpy as np
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from IPython.display import Image
+from keras.models import load_model
+from keras.preprocessing import image
+
+emotion_model = load_model("Model/Output/Emotion_Model.keras")
+gender_model = load_model("Model/Output/Gender_Model.keras")
+
+#Creating two class dictionary
+emotion_classes = {0:'Happy', 1:'Sad'}
+gender_classes = {0:'Male', 1:'Female'}
+
+#Read downloaded test image in Opencv
+test_img = cv2.imread('Model/Photos/1happy.png')
+test_img = cv2.imread('Model/Photos/1sad.png')
+
+
+#Convert image to gray scale OpenCV
+gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+
+#Define haar cascade classifier for face detection
+face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+  
+#Detect face using haar cascade classifier
+faces_coordinates = face_classifier.detectMultiScale(gray_img, 1.3, 5)
+ 
+i = 0
+#Draw a rectangle around the faces
+for (x, y, w, h) in faces_coordinates:
+    i = i + 1
+    #Draw rectangle around face
+    cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+         
+    #Crop face from image
+    cropped_face = gray_img[y:y+h, x:x+w]
+
+    final_image1 = cv2.resize(cropped_face, (224, 224), interpolation = cv2.INTER_AREA)
+    final_image_array = np.array(final_image1)
+    input_test = np.expand_dims(final_image_array, axis = 0) ## Need 4th dimension
+    output_test = emotion_classes[np.argmax(emotion_model.predict(input_test))]
+    output_str = str(i) + ": " + output_test
+    print(output_str)
+    
+    #Define opencv font style
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    col = (0, 0, 255)
+    cv2.putText(test_img, output_test, (x, y), font, 2, col, 2)
+    
+    #Crop face from image
+    cropped_face2 = gray_img[y:y+h, x:x+w]
+
+    final_image2 = cv2.resize(cropped_face2, (224, 224), interpolation = cv2.INTER_AREA)
+    final_image_array2 = np.array(final_image2)
+    input_test2 = np.expand_dims(final_image_array2, axis = 0) ## Need 4th dimension
+    output_test2 = gender_classes[np.argmax(gender_model.predict(input_test2))]
+    output_str2 = str(i) + ": " + output_test2
+    print(output_str2)
+    
+    #Define opencv font style
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    col = (0, 0, 255)
+    cv2.putText(test_img, output_test2, (x, y + h + 25), font, 2, col, 2)
+     
+plt.imshow(cv2.cvtColor(test_img,cv2.COLOR_BGR2RGB))
+plt.show()
---
+++ Model/testing live video.py
@@ -0,0 +1,65 @@
+#Import Dependencies
+import cv2
+import numpy as np
+from tensorflow import keras
+from keras.models import load_model
+
+emotion_model = load_model("Model/Output/Emotion_Model.keras")
+gender_model = load_model("Model/Output/Gender_Model.keras")
+
+emotion_classes = {0:'Happy', 1:'Sad'}
+gender_classes = {0:'Male', 1:'Female'}
+
+#Load face cascade classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+#Start capturing video
+cap = cv2.VideoCapture(0)
+
+while True:
+    #Capture frame-by-frame
+    ret, frame = cap.read()
+
+    #Convert frame to grayscale
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #Detect faces in frame
+    faces = face_cascade.detectMultiScale(gray_frame,1.3,5)
+
+    for (x, y, w, h) in faces:
+        #Extract face ROI (Region of Interest)
+        face_roi = gray_frame[y:y + h, x:x + w]
+        #Resize image to match model's expected sizing
+        img = cv2.resize(face_roi ,(224,224))     
+        img = img.reshape(-1,224,224,1) 
+        
+        #Extract face ROI (Region of Interest)
+        face_col = gray_frame[y:y + h, x:x + w]
+        #Resize image to match model's expected sizing
+        img2 = cv2.resize(face_col ,(224,224))
+        img2 = img2.reshape(-1,224,224,1)
+        
+        #Perform emotion and gender analysis on face ROI
+        #Determine emotion and gender
+        predicted_emotion = emotion_model.predict(img)
+        predicted_gender = gender_model.predict(img2)
+        
+        index = int(np.argmax(predicted_emotion))
+        index2 = int(np.argmax(predicted_gender))
+
+        #Draw rectangle around face and label with predicted emotion and gender
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(frame, emotion_classes[index], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+        cv2.putText(frame, gender_classes[index2], (x, y + h + 25), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+        
+    #Display resulting frame
+    cv2.imshow('Real-time Emotion and Gender Detection', frame)
+
+    #Press 'q' to exit
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+#Release capture and close all windows
+cap.release()
+cv2.destroyAllWindows()
